cb994c6741e44d4f5400860d86eff3210b081555
Nandini Chaudhary <30980878+Nancy961@users.noreply.github.com>
2020-04-27 14:11:30 +0530

    Compilers and Parsers

---
+++ Practice/DSA/2: Linear Data Structures - Vectors, Pairs, Linked List, Stacks, Queue/COMPILER.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;  
+    cin>>t;
+    while(t--)
+    {
+      string s;   
+      cin>>s;
+      long count=0,ans=0;
+      for(long long i=0;i<s.length();i++)
+      {
+        if(s[i]=='<')	
+          count++;
+        else            	
+          count--;
+
+        if(count<0) 
+          break;
+        if(count==0)
+          ans=i+1;
+      }
+      cout<<ans<<endl;
+    }
+}

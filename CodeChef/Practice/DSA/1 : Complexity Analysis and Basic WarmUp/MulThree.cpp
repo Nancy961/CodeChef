0637a395c21cb682ffb362051b36fc7764c38379
Nandini Chaudhary <30980878+Nancy961@users.noreply.github.com>
2020-05-14 20:56:56 +0530

    Multiple of Three (for small value of k)

---
+++ CodeChef/Practice/DSA/1 : Complexity Analysis and Basic WarmUp/MulThree.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int T; cin>>T;
+    while(T--)
+    {
+        long int k, d0, d1;
+        cin>>k>>d0>>d1;
+        long int sum= d0+d1;
+
+        for(int i=2;i<k;i++)
+        {
+            long int digit = 0;
+            digit +=sum%10;
+            sum += digit;
+        }
+        if(sum%3 == 0) cout<<"YES";
+        else cout<<"NO";
+    }       
+    return 0;
+}
